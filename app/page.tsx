60153fd9973497114a9d749de97b3689e5aed7bd
Aruberth <alberthqwertyk@gmail.com>
2025-12-29 17:54:47 -0300

    refactor: remove Prisma configuration and schema files; add Button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import { Welcome } from "@/components/ui/welcome_page/welcome";
-
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Welcome></Welcome>
     </div>
   );
 }
