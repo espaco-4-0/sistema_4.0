8c4ac57008266d61352b7bb0eb9f7ae1a7b2afd6
Aruberth <alberthqwertyk@gmail.com>
2025-12-29 16:05:08 -0300

    Add TypeScript configuration file (tsconfig.json) with strict settings and module resolution

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Welcome } from "@/components/ui/welcome_page/welcome";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Welcome></Welcome>
+    </div>
+  );
+}
