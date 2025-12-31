78844d757bc074cc0fb14ff207bf116256055921
gustavo-lola <gustavolola.dev@gmail.com>
2025-12-31 12:39:47 -0300

    Criacao da pagina de blog seguindo fidedignamente ao figma, seguindo ate os detalhes de UX/UI adicionando movimentacoes e transicoes de noticias acerca de 1 min elas vao se alterando juntamente com a interatividade de hover: ao passar o mouse pela noticia principal ou noticias subjacentes...

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client"
+import Blog from "@/ui/components/proprietary/landing_page/blog";
 import { Welcome } from "@/ui/components/proprietary/landing_page/welcome";
 
 export default function Home() {
   return (
     <div>
       <Welcome/>
+      <Blog/>
     </div>
   );
 }
