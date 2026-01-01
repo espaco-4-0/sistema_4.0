36cd2717b5ff99d1549cd41ed66e145352e8b80b
gustavo-lola <gustavolola.dev@gmail.com>
2026-01-01 15:41:10 -0300

    Criacao do componente oq e o espaco 4.0 para que seja de facil entendimento a quem visitar o site e ver oq e de fato o espaco4.0, adicao do component CARD+Shadcn e tbm da Imagem-4 Seguindo o figma alem dos parametros delee

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 import { Welcome } from "@/ui/components/proprietary/landing_page/welcome";
+import WhatIsSpace from "@/ui/components/proprietary/landing_page/what_is";
 
 export default function Home() {
   return (
     <div>
       <Welcome/>
+      <WhatIsSpace/>
     </div>
   );
 }
