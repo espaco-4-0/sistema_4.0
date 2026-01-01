ec449ad40be9d328709912b01fee340c42d66429
gustavo-lola <gustavolola.dev@gmail.com>
2026-01-01 20:38:23 -0300

    foi foi foi

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Blog from "@/ui/components/proprietary/landing_page/blog";
 import { Welcome } from "@/ui/components/proprietary/landing_page/welcome";
-import { WhatIsSpace } from "@/ui/components/proprietary/landing_page/what_is"
+import  WhatIsSpace  from "@/ui/components/proprietary/landing_page/what_is"
 export default function Home() {
   return (
     <div>
