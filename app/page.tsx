152ff73d7fdff061d8f31270095cf4d87d4c7006
Aruberth <alberthqwertyk@gmail.com>
2026-01-02 10:45:42 -0300

    Add Upcoming Events component and update Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 "use client"
 import Blog from "@/ui/components/proprietary/landing_page/blog";
 import { Welcome } from "@/ui/components/proprietary/landing_page/welcome";
-import  WhatIsSpace  from "@/ui/components/proprietary/landing_page/what_is"
+import WhatIsSpace from "@/ui/components/proprietary/landing_page/what_is";
+import { UpcomingEvents } from "@/ui/components/proprietary/landing_page/upcoming_events";
+
 export default function Home() {
-  return (
-    <div>
-      <Welcome/>
-      <Blog/>
-      <WhatIsSpace/>
-    </div>
-  );
+    return (
+        <div>
+            <Welcome/>
+            <Blog/>
+            <WhatIsSpace/>
+            <UpcomingEvents/>
+        </div>
+    );
 }
