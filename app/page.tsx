d1a2ca793b50a2a58df29d972f12ec7cf77d7d09
gustavo-lola <gustavolola.dev@gmail.com>
2026-01-03 13:33:30 -0300

    Desenvolvimento da pagina de Cursos e Treinos

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { Welcome } from "@/ui/components/proprietary/landing_page/welcome";
 import WhatIsSpace from "@/ui/components/proprietary/landing_page/what_is";
 import { UpcomingEvents } from "@/ui/components/proprietary/landing_page/upcoming_events";
 import ComeAndDiscover from "@/ui/components/proprietary/landing_page/come_and_discover";
+import CoursesAndTraining from "@/ui/components/proprietary/landing_page/couses_and_training";
 
 export default function Home() {
     return (
@@ -11,6 +12,7 @@ export default function Home() {
             <Welcome/>
             <Blog/>
             <WhatIsSpace/>
+            <CoursesAndTraining/>
             <UpcomingEvents/>
             <ComeAndDiscover/>
         </div>
