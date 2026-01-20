8ba12d298bad5702226856984ab71a5255b59233
gustavo-lola <gustavolola.dev@gmail.com>
2026-01-20 11:54:23 -0300

    Criacao da tela de loading principal

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,13 +1,27 @@
-import { Cat } from "lucide-react";
+"use client"
+import { useState, useEffect } from "react";
+import { Cog } from "lucide-react";
 
 export default function Loading() {
+  const [i, setI] = useState(0);
+
+  useEffect(() => {
+    if (i >= 100) return;
+
+    const timer = setTimeout(() => {
+      setI(i + 1);
+    }, 30);
+
+    return () => clearTimeout(timer);
+  }, [i]);
+
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center bg-white gap-8">
+    <div className="flex flex-col min-h-screen items-center justify-center backdrop-blur-md gap-8">
       <div className="relative flex items-center justify-center w-24 h-24">
-        <div className="absolute inset-0 border-2 border-dashed border-slate-400 rounded-full animate-[spin_8s_linear_infinite]"></div>
+        <div className="absolute inset-0 border-2 border-dashed border-black rounded-full animate-[spin_8s_linear_infinite]"></div>
         <div className="absolute w-20 h-20 border border-slate-100 rounded-full"></div>
-        <div className="text-slate-700 animate-bounce">
-          <Cat size={40} strokeWidth={1.5} />
+        <div className="text-yellow-icon-light animate-[spin_8s_linear_infinite]">
+          <Cog size={40} strokeWidth={1.5} />
         </div>
       </div>
 
@@ -15,6 +29,11 @@ export default function Loading() {
         <span className="text-slate-600 font-medium tracking-widest uppercase text-xs">
           Carregando
         </span>
+
+        <span className="text-slate-800 font-mono font-bold text-lg">
+          {i}%
+        </span>
+
         <div className="flex gap-1">
           <span className="w-1.5 h-1.5 bg-slate-400 rounded-full animate-bounce [animation-delay:-0.3s]"></span>
           <span className="w-1.5 h-1.5 bg-slate-400 rounded-full animate-bounce [animation-delay:-0.15s]"></span>
---
+++ src/app/slow-page/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+import { useEffect, useState } from "react"
+import LoadingCat from "../loading"
+
+export default function SlowPage(){
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setLoading(false), 10000);
+        return () => clearTimeout(timer);
+    }, []);
+
+    if(loading) return <LoadingCat />
+
+    return(
+        <div>
+            <h1>Página Pronta</h1>
+        </div>
+    )
+}
