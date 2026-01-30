4136c14ab6c8594bcff7b9b6bfe90f5e267d9492
JoaoGSantiago <gustavo2jg@gmail.com>
2026-01-30 12:10:14 -0300

    chore: adicionando nova tela ao clicar o botao, deletando arquivos do fluxo de reset, mudando botao para o shadcn e borda do cadeado

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,26 +1,9 @@
 "use client";
 
-import { Suspense } from "react";
-import RecoveryPasswordForm from "@/src/ui/modules/auth_pages/recovery-password-form";
+import ResetPasswordForm from "@/src/ui/modules/auth_pages/reset-form";
 
-// renderiza o formulario
 function ResetPasswordContent() {
-  // token ficticio
-  const mockToken = "dev-mock-token-123";
-  return <RecoveryPasswordForm token={mockToken} />;
+  return <ResetPasswordForm />;
 }
 
-// Página principal
-export default function ResetPasswordPage() {
-  return (
-    <Suspense
-      fallback={
-        <div className="flex items-center justify-center min-h-screen">
-          Carregando...
-        </div>
-      }
-    >
-      <ResetPasswordContent />
-    </Suspense>
-  );
-}
+export default ResetPasswordContent;
