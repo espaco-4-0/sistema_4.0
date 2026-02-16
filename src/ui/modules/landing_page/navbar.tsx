7494257d8e7f80b989499352e7699ddc5c72e9bc
gustavo-lola <gustavolola.dev@gmail.com>
2026-02-16 20:40:39 -0300

    criacao da refatoracao da navbar inicio

--- src/ui/modules/landing_page/navbar.tsx
+++ src/ui/modules/landing_page/navbar.tsx
@@ -8,7 +8,7 @@ import {
     NavigationMenuLink,
     NavigationMenuList,
 } from "@radix-ui/react-navigation-menu";
-import { Menu, X } from "lucide-react";
+import { Calendar, Menu, Play, User, X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -21,22 +21,25 @@ export default function NavBar() {
     };
 
     const menuItems = [
+        { id: "inicio", label: "Inicio" },
         { id: "what-is", label: "Sobre" },
+        { id: "tecnologias", label: "Tecnologias" },
         { id: "blog", label: "Notícias" },
         { id: "courses", label: "Cursos" },
         { id: "upcoming-events", label: "Eventos" },
+        { id: "galeria", label: "Galeria" },
         { id: "footer", label: "Contato" },
     ];
 
     return (
-        <NavigationMenu className="fixed top-0 z-20 w-full bg-white">
-            <div className="relative flex h-20 items-center justify-center px-6 lg:px-12">
-                <Button
-                    variant="ghost"
-                    className="absolute left-6 p-0 cursor-pointer"
-                    onClick={() => automaticScroll("welcome")}
-                >
+        <NavigationMenu className="fixed top-0 z-4 w-full bg-white">
+            <div className="relative flex h-20 items-center justify-center px-3 lg:px-12">
+                <Button variant="ghost" className="flex cursor-pointer" onClick={() => automaticScroll("welcome")}>
                     <Image src="/Icone-Espaco4.0.svg" alt="Logo do Espaço 4.0" width={48} height={48} />
+                    <div className="items-start flex flex-col">
+                        <h1 className="text-black font-bold items-start">Espaco 4.0</h1>
+                        <p className="text-gray-400 font-semibold items-center">Tecnologia & Educação</p>
+                    </div>
                 </Button>
 
                 <NavigationMenuList className="hidden lg:flex gap-3">
@@ -55,13 +58,21 @@ export default function NavBar() {
                     ))}
                 </NavigationMenuList>
 
+                <Link href="#3D" className="flex text-[14px] gap-4">
+                    <Play /> Ver em 3D
+                </Link>
+                <div className="p-3 gap-2">
+                    <Link href="" className="gap-2 flex bg-amber-400 p-1 text-[14px] border rounded-xl">
+                        <Calendar className="text-14px" /> Agendar Visita
+                    </Link>
+                </div>
+                <p className="p-4 text-gray-500 ">|</p>
                 <Link
                     href="/login-page"
-                    className="absolute right-12 hidden lg:flex h-9 w-22 items-center justify-center rounded bg-black text-white hover:bg-black/80"
+                    className="flex lg:flex h-9 w-22 items-center justify-center rounded text-black border-2"
                 >
-                    Entrar
+                    <User className="gap-2" /> Entrar
                 </Link>
-
                 <Button
                     variant="ghost"
                     size="icon"
