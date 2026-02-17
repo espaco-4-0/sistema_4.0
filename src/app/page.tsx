e87f629fa34dc687349a47f13667e6a0d1614f3d
gustavo-lola <gustavolola.dev@gmail.com>
2026-02-17 11:53:15 -0300

    criacao da pagina de galeria do espaco 4.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import ComeAndDiscover from "../ui/modules/landing_page/come_and_discover";
 import CoursesAndTraining from "../ui/modules/landing_page/courses_and_training";
 import Footer from "../ui/modules/landing_page/footer";
 import NavBar from "../ui/modules/landing_page/navbar";
+import SpaceGallery from "../ui/modules/landing_page/space_gallery";
 import { UpcomingEvents } from "../ui/modules/landing_page/upcoming_events";
 import { Welcome } from "../ui/modules/landing_page/welcome";
 import WhatIsSpace from "../ui/modules/landing_page/what_is";
@@ -19,6 +20,7 @@ export default function Home() {
             <CoursesAndTraining />
             <UpcomingEvents />
             <ComeAndDiscover />
+            <SpaceGallery />
             <Footer />
         </div>
     );
---
+++ src/ui/modules/landing_page/space_gallery.tsx
@@ -0,0 +1,14 @@
+export default function SpaceGallery() {
+    return (
+        <section id="space_gallery" className="bg-[#F9FAFB] items-center py-20">
+            <h2 className="text-4xl font-semibold text-center">
+                Galeria do <span className="text-yellow-muted font-semibold">Espaco 4.0</span>
+            </h2>
+            <p className="mt-5 mb-17 text-xl text-center text-gray-600">
+                Veja momentos Reais de aprendizado, criação e inovação
+            </p>
+
+            <div className="grid grid-cols-4"></div>
+        </section>
+    );
+}
